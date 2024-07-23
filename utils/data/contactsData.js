69aa5e58e8e480d942e375adcf09fd35e196d24c
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-07-23 17:03:01 +0500

    updated

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,7 +3,7 @@ export const contactsData = {
     phone: '+923132297998',
     address: 'Islamabad, Pakistan',
     github: 'https://github.com/mehdikhan55',
-    facebook: 'https://www.facebook.com/',
+    facebook: 'https://www.facebook.com/mehdiali55/',
     linkedIn: 'https://www.linkedin.com/in/mehdikhan55/',
     twitter: 'https://twitter.com/',
     stackOverflow: 'https://stackoverflow.com/',
